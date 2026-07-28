1b3e231cecac5cd7205135a039d0ede04ed37063
digitdos-lab <digitdos@gmail.com>
2026-07-28 12:28:30 +0900

    Update MayaArnoldRenderSettings.py

--- MayaArnoldRenderSettings.py
+++ MayaArnoldRenderSettings.py
@@ -2,14 +2,23 @@ import os
 
 import maya.cmds as cmds
 
+from maya import OpenMayaUI
+
 from PySide2 import QtWidgets
 from Pysode2 import QtCore
 
 from shiboken2 import wrapInstance
 
-cmds.SetAttr("defaultArnoldRenderOptions.AASamples", 5)
-cmds.SetAttr("defaultArnoldRenderOptions.GIDiffuseSamples", 2)
-cmds.SetAttr("defaultArnoldRenderOptions.GISpeculaSamples", 2)
-cmds.SetAttr("defaultArnoldRenderOptions.GITransmission", 2)
-cmds.SetAttr("defaultArnoldRenderOptions.GISssSamples", 5)
-cmds.SetAttr("defaultArnoldRenderOptions.GIVolumeSamples", 2)
+cmds.setAttr("defaultArnoldRenderOptions.AASamples", 4)
+cmds.setAttr("defaultArnoldRenderOptions.GIDiffuseSamples", 2)
+cmds.setAttr("defaultArnoldRenderOptions.GISpeculaSamples", 2)
+cmds.setAttr("defaultArnoldRenderOptions.GITransmission", 2)
+cmds.setAttr("defaultArnoldRenderOptions.GISssSamples", 5)
+cmds.setAttr("defaultArnoldRenderOptions.GIVolumeSamples", 2)
+
+
+lights = cmds.ls(light=True)
+
+for light in lights :
+  cmds.setAttr("[light].aiSamples", 2)
+  cmds.setAttr("[light].exposure", 8)
